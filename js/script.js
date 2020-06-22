715152d6b8f4f13605b9cdb6c3448bab5511be35
Tyler Degand <tylerdegand@gmail.com>
2020-06-21 22:18:13 -0500

    Added the data needed in an Array

--- js/script.js
+++ js/script.js
@@ -4,13 +4,36 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 // For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {
+    quote: 'History repeats itself, first as tragedy, second as farce.',
+    author: '-Karl Marx'
+  },
+  {
+    quote: 'Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.',
+    author: '-Mark Twain'
+  },
+  {
+    quote: 'Holding on to anger is like grasping a hot coal with the intent of throwing it at someone else; you are the one who gets burned.',
+    author: 'Buddha',
+    year: '563 BC - 483 BC'
+  },
+  {
+    quote: 'It is our choices, that show what we truly are, far more than our abilities.',
+    author: '– J. K Rowling'
+  },
+  {
+    quote: 'Deep into that darkness peering, long I stood there, wondering, fearing, doubting, dreaming dreams no mortal ever dared to dream before.',
+    author: 'Edgar Allen Poe',
+    citation: 'https://www.poetryfoundation.org/poems/48860/the-raven'
+  }
+];
 
 
 /***
